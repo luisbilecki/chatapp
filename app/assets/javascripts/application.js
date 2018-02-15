23a75dc481d36ebc0b0a0aa098de791320d071f3
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-02-15 16:57:41 -0200

    Add flash alerts based on bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootbox
+//= require bootstrap-notify
 //= require turbolinks
 //= require cable
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{opts.delete(:size) { 40 }}",
               opts
   end
+
+  def flash_class(level)
+    case level
+        when 'notice' then 'alert alert-info'
+        when 'success' then 'alert alert-success'
+        when 'error' then 'alert alert-error'
+        when 'alert' then 'alert alert-error'
+    end
+  end
 end
